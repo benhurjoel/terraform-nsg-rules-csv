46bfd72bf4664cee30202194ebaded10ddbe8da9
Ben <57881441+benhurjoel@users.noreply.github.com>
2022-07-25 11:50:36 +0800

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "test-resource-group"
   location = "South EastAsia"
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "TestSecurityGroup1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+}
